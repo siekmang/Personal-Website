995aedb0f8355b135927830573a4bf300fca2987
Greg <github@siekmang.com>
2026-07-14 16:43:49 -0400

    Ported to Astro
    Website ported to Astro. Right now it's just adapted to Astro's format. I still want to make it into components and layouts and convert the css to Tailwind.

---
+++ src/js/gen/nametagGen.js
@@ -0,0 +1,27 @@
+import { activePages } from "../globals.js";
+
+export function nametagGen() {
+  let nametag = document.querySelector("#nametag");
+  let header = `<h1>GREG SIEKMAN</h1>`;
+
+  var page = window.location.pathname.split("/").pop();
+  if (page === "") page = "index";
+
+  if (page == "privacy.html" || page == "privacy") {
+    header = `<h1 class="privacy-h1">PRIVACY POLICY</h1>`;
+  } else if (!activePages.includes(page)) {
+    header = "";
+  }
+
+  nametag.innerHTML = `
+    ${header}
+    <div class="menu-pill">
+        <ul class="site-nav">
+            <li class="site-nav-item"><a href="index.html" rel="noopener noreferrer">Home</a></li>
+            <li class="site-nav-item"><a href="portfolio.html" rel="noopener noreferrer">Things</a></li>
+            <li class="site-nav-item"><a href="https://blog.siekmang.com" rel="noopener noreferrer">Blog</a></li>
+            <li class="site-nav-item"><a href="privacy.html" rel="noopener noreferrer">Privacy Policy</a></li>
+        </ul>
+    </div>
+  `;
+}
---
+++ src/js/handlers/linksHandler.js
@@ -0,0 +1,82 @@
+export function linksHandler() {
+  //These are variables for the below functions that handle the more/less button on the links page
+  const linksMore = document.getElementById("links-more-button");
+  const linksMoreDiv = document.getElementById("link-more");
+
+  //This sets the initial state of the more links div to none
+  if (linksMore) {
+    linksMoreDiv.style.display = "none";
+
+    // This handles the more/less button on the links page. It checks for a pointerdown event
+    // and then checks the current state of the linksMoreDiv. If it's none, it sets it to grid
+    // and changes the button text to less. If it's not none, it sets it to none and changes
+    // the button text to more. It also prevents default behavior like text selection and
+    // smoothly scrolls back to the current scroll position.
+    linksMore.addEventListener("pointerdown", () => {
+      const currentScroll = window.scrollY; // Get current scroll position
+
+      if (linksMoreDiv.style.display === "none") {
+        linksMoreDiv.style.display = "grid";
+        linksMore.innerHTML = "<p>Less &uarr;</p>";
+      } else {
+        linksMoreDiv.style.display = "none";
+        linksMore.innerHTML = "<p>More &darr;</p>";
+        window.scrollTo({ top: currentScroll, behavior: "smooth" }); // Smoothly adjust scroll
+      }
+    });
+  }
+
+  // These are variables for the below functions that handle link buttons on the links page.
+  const links = document.querySelectorAll(".link-menu-div");
+  const linksSub = document.querySelectorAll(".link-submenu-div");
+
+  // This handles button behavior on my links page. The first if checks for
+  // the user using a device with a fine poiner(not a touchscreen device), and then sets
+  // some constants.
+  if (window.matchMedia("(pointer: fine)").matches) {
+    const hoverStyle = {
+      transform: "scale(1.06)",
+      transition: "all .2s",
+      backgroundColor: "var(--link-button-hover-color)",
+    };
+    const defaultStyle = {
+      transform: "scale(1)",
+      transition: "all .2s",
+      backgroundColor: "var(--menu-pill-color)",
+    };
+    const clickStyle = {
+      transform: "scale(1)",
+      transition: "all .01s",
+    };
+
+    // This is where it listens for different mouse events and changes
+    // style based on those events
+    links.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        Object.assign(link.style, hoverStyle);
+      });
+
+      link.addEventListener("mouseout", () => {
+        Object.assign(link.style, defaultStyle);
+      });
+
+      link.addEventListener("mousedown", () => {
+        Object.assign(link.style, clickStyle);
+      });
+    });
+
+    linksSub.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        Object.assign(link.style, hoverStyle);
+      });
+
+      link.addEventListener("mouseout", () => {
+        Object.assign(link.style, defaultStyle);
+      });
+
+      link.addEventListener("mousedown", () => {
+        Object.assign(link.style, clickStyle);
+      });
+    });
+  }
+}
